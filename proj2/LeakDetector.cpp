8521b2470a88254a637e43ebfd5e08ecc6522c65
Pedro Abreu <abreu223@hotmail.com>
2023-11-01 15:50:42 -0400

    First version working

--- proj2/LeakDetector.cpp
+++ proj2/LeakDetector.cpp
@@ -29,39 +29,79 @@ using namespace llvm;
 
 std::list<std::list<BasicBlock *>> paths;
 // or std::list<std::list<Instruction *>> paths;
+
+void PrintPath(std::list<BasicBlock *> path){
+    int lineno = 0;
+    int prev_line = 0;
+    for (auto bb: path) {
+        for (Instruction& i : *bb) {
+            if ((&i)->getDebugLoc()) {
+                int lineno = (&i)->getDebugLoc().getLine();
+                if (prev_line != lineno){
+                    outs() << lineno;
+                    // Check if this is the last instruction
+                    // if(!(&i)->isTerminator())
+                    outs() << " -> ";
+                    prev_line = lineno;
+                }
+            }
+        }
+    }
+    outs() << "\n";
+}
+
 void DiscoverPaths(std::list<BasicBlock *> path, BasicBlock *bb)
 {
     path.push_back(bb);
+    // outs() << "entrou Func\n";
     for (Instruction &i : (*bb))
     {
-        std::list<BasicBlock *> path
-            BranchInst *BI = dyn_cast<BranchInst>(&i);
+        BranchInst *BI = dyn_cast<BranchInst>(&i);
         if (BI)
         {
-
-            // write your code
-            // You may need to do a recursive call
-            // for (int j = 0 ; j < BI->getNumSuccessors(); j++)
-            //{
-            //   BasicBlock * target = BI->getSuccessor(j);
-            //
-            //    ...
-            //}
+            for (int j = 0 ; j < BI->getNumSuccessors(); j++)
+            {
+                // outs() << "entrou For" << j << "\n";
+                BasicBlock * target = BI->getSuccessor(j);
+                DiscoverPaths(path, target);
+            }
+            return;
         }
     }
+    // outs() << "saiu Func\n";
     paths.push_back(path);
     return;
 }
 
+void print_aliases(std::map<std::string, std::string> aliases){
+    for (auto alias : aliases){
+        outs() << alias.first << " = " << alias.second << ", ";
+
+    }
+    outs() << "\n";
+}
+
 bool MemoryLeakDetection(std::list<BasicBlock *> path)
 {
-
+    std::map<std::string, Instruction*> malloc_vars;
+    malloc_vars.clear();
+    std::map<std::string, std::string> aliases;
     for (auto bb: path) {
         for (Instruction& i : *bb) {
 
             LoadInst *LI = dyn_cast<LoadInst>(&i);
             if(LI){
                 // write your code
+                Instruction *inst = (&i)->getNextNonDebugInstruction();
+                if(inst){
+                    StoreInst *nextStore = dyn_cast<StoreInst>(inst);
+
+                    if(nextStore){
+                        std::string alias = nextStore->getOperand(1)->getName().str();
+                        std::string var = LI->getOperand(0)->getName().str();
+                        aliases[alias] = var;
+                    }
+                }
             }
             
 
@@ -72,21 +112,54 @@ bool MemoryLeakDetection(std::list<BasicBlock *> path)
 
                 // Example of accessing to getOperand()
                 if(DEBUG){
-                    outs() << "[DEBUG][STORE] "  << i << "\n";
-                    outs() << "[DEBUG][STORE] "  << SI->getOperand(1) << "\n";
+                    //outs() << "[DEBUG][STORE] "  << i << "\n";
+                    //outs() << "[DEBUG][STORE] "  << SI->getOperand(1) << "\n";
                 }
             }
              
             CallInst * CI = dyn_cast<CallInst>(&i);
             if (CI){ 
                 if (CI->getCalledFunction()->getName().find("malloc") != std::string::npos) {
-                    // Write your code
-                    if(DEBUG){
-                        outs() << "Found Malloc\n";
+                    StoreInst *nextStore = dyn_cast<StoreInst>((&i)->getNextNonDebugInstruction());
+
+                    if(nextStore){
+                        std::string var = nextStore->getOperand(1)->getName().str();
+                        malloc_vars.insert({var, &i});
+
+                        // Write your code
+                        if(DEBUG){
+                            // outs() << i << "\n";
+                            // outs() << *nextStore << "\n";
+                            // outs() << var << "\n";
+                        }
                     }
 
                 }
                 if (CI->getCalledFunction()->getName().find("free") != std::string::npos) {
+                    LoadInst *previousLI = dyn_cast<LoadInst>((&i)->getPrevNonDebugInstruction());
+
+                    if(previousLI){
+                        std::string var = previousLI->getOperand(0)->getName().str();
+
+                        // erase var, or it's alias
+                        // print_aliases(aliases);
+                        do{
+                            // outs() << var << "\n";
+                            int n = malloc_vars.erase(var);
+                            // didn't erase var
+                            if(n == 0) {
+                                if (aliases.count(var) < 1)
+                                    break;
+                                var = aliases.at(var);
+                            } else break;
+                        } while(1);
+
+                        if(DEBUG){
+                            // outs() << *previousLI << "\n";
+                            // outs() << var << "\n";
+                            // outs() << i << "\n";
+                        }
+                    }
                     // Write your code
                     
                 }           
@@ -97,6 +170,9 @@ bool MemoryLeakDetection(std::list<BasicBlock *> path)
     /*
      * return True of False 
      */
+    if(malloc_vars.empty())
+        return false;
+
     return true;
 }
 
@@ -121,21 +197,12 @@ struct LeakDetector : public ModulePass {
         DiscoverPaths(path, &(fun.front()));
         // outs() << "Paths running\n";
 
-        for (auto p: paths) 
-        { 
-            for (auto bb: p) {
-
-                for (Instruction& i : *bb) {
-
-                    if ((&i)->getDebugLoc()) {
-                        int lineno = (&i)->getDebugLoc().getLine();
-                        outs() << lineno << " -> ";
-                    }
-                }
-            }
-            outs() << "\n";
+        for (auto p: paths) { 
+            // PrintPath(p);
+            bool b = MemoryLeakDetection(p);
+            if (b)
+                PrintPath(p);
         }
-    
     }
   return false; 
   } 
